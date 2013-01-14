1b231edc962731c9c313d42886f4cfcfe7866d20
ryanp <ryanp@doapps.com>
2013-01-14 16:50:29 -0600

    initial version

---
+++ aecad.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+*
+* elasticache auto-discovery cron - async update of active servers for PHP clients
+*
+* @see https://github.com/rynop/aecad-for-php
+*
+* @license      MIT License (http://www.opensource.org/licenses/mit-license.php)
+*
+**/
+define("KEYNAME", "ec_servers");
+
+if(empty($_GET['server'])) {
+	echo "no servers";
+	exit();
+}
+
+//Put servers in format for PECL memcached addServers: http://php.net/manual/en/memcached.addservers.php
+$ecServers = array();
+foreach($_GET['server'] as $server){
+	$pieces = explode('|', $server,3);
+	$ecServers[] = array($pieces[0],$pieces[1],100);
+}
+
+if(!empty($ecServers)) {
+	apc_store(KEYNAME,$ecServers);
+}
+
+/** for debug
+$s = apc_fetch(KEYNAME);
+var_export($s);
+**/
---
+++ aecad.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+####
+#
+# elasticache auto-discovery cron - async update of active servers for PHP clients
+#
+# @see https://github.com/rynop/aecad-for-php
+#
+# @license      MIT License (http://www.opensource.org/licenses/mit-license.php)
+#
+####
+
+CLUSTER_NAME="my-cluster"
+CONFIGURATION_ENDPOINT="${CLUSTER_NAME}.1234.cfg.use1.cache.amazonaws.com"
+CONFIGURATION_ENDPOINT_PORT="11211"
+LOCAL_WS_PORT="49000"
+
+#cluster config output is in the format:
+#
+#CONFIG cluster 0 146
+#1
+#<CONFIGURATION_NAME>.blah|10.1.1.1|11211 <CONFIGURATION_NAME>.blah|10.2.2.2|11211
+
+output=`echo 'config get cluster' | nc $CONFIGURATION_ENDPOINT $CONFIGURATION_ENDPOINT_PORT`
+
+QS=""
+for server in $output; do 
+
+	echo "$server" | grep "$CLUSTER_NAME" > /dev/null
+	if [ $? -eq 0 ]; then
+
+		QS+="server%5B%5D=${server}&"
+		#If for some reason you want to pass the parts up to your web service
+		#	IFS='|' read -ra PARTS <<< "$server"
+		#	server=${PARTS[0]}
+		#	ip=${PARTS[1]}
+		#	port=${PARTS[2]}
+		#	echo ${server}:${port}
+	fi
+done
+
+if [ !-z "$QS" ]; then	
+	curl -s "localhost:${LOCAL_WS_PORT}/aecad.php?${QS}"
+fi
+
